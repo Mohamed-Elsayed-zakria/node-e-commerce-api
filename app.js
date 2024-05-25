1658eb721eda71a9879aa2b4ed7e6ecbeb45d71b
Mohamed Elsayed Zakria Saied <hagatonline24@gmail.com>
2024-05-25 14:11:25 +0300

    Connect database

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const morgan = require("morgan");
 dotenv.config({ path: "config.env" });
@@ -8,13 +9,18 @@ const port = process.env.PORT;
 
 if (process.env.NODE_ENV === "development") {
     app.use(morgan("dev"));
-    console.log(`mode : ${process.env.NODE_ENV}`);
+    console.log(`mode ==> ${process.env.NODE_ENV}`);
 }
 
 app.get("/", (req, res) => {
     res.send("E commerce app v1.0.0");
 });
 
-app.listen(port, () => {
-    console.log(`http://localhost:${port}`);
+mongoose.connect(process.env.db_url).then((result) => {
+    console.log(`database connected : ${result.connection.host}`);
+    app.listen(port, () => {
+        console.log(`http://localhost:${port}`);
+    });
+}).catch((err) => {
+    console.log(err);
 });
