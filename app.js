09cb220a2a52eb488e253feb1289082e9b9308c7
Mohamed Elsayed Zakria Saied <hagatonline24@gmail.com>
2024-05-25 12:54:43 +0300

    add morgan middlware

--- app.js
+++ app.js
@@ -1,9 +1,15 @@
 const express = require("express");
 const dotenv = require("dotenv");
+const morgan = require("morgan");
 dotenv.config({ path: "config.env" });
 
 const app = express();
 const port = process.env.PORT;
+
+if (process.env.NODE_ENV === "development") {
+    app.use(morgan("dev"));
+    console.log(`mode : ${process.env.NODE_ENV}`);
+}
 
 app.get("/", (req, res) => {
     res.send("E commerce app v1.0.0");
